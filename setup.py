a96441fd2e66a5bb12799166c5ce162f67c7bb81
Behrouz <behrouz.safari@gmail.com>
2021-07-26 21:51:41 +0200

    ver 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sdss",
-    version="0.1.4",
+    version="0.1.6",
     author="Behrouz Safari",
     author_email="behrouz.safari@gmail.com",
     description="A python package for retrieving and analysing data from SDSS (Sloan Digital Sky Survey)",
